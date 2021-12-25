c85c0acb1477a426887c371a2fd1214f0923b954
zhenl <zhenlei1970@hotmail.com>
2021-12-25 20:55:27 +0800

    Add fanout

---
+++ src/FanoutReceiver1/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "127.0.0.1",
+    UserName = "admin",
+    Password = "admin",
+    VirtualHost = "my_vhost"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "example.exchange",
+type: "fanout", durable: true);
+
+    channel.QueueDeclare(queue: "que1",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    channel.QueueBind(queue: "que1", exchange: "example.exchange",
+routingKey: "");
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine("收到消息 {0}", message);
+    };
+    channel.BasicConsume(queue: "que1",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" 按回车退出");
+    Console.ReadLine();
+}
