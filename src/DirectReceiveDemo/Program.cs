52493993ce4ede82a3f86f2401126abffd5be9cf
zhenl <zhenlei1970@hotmail.com>
2021-12-25 14:37:47 +0800

    Direct

---
+++ src/DirectReceiveDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "127.0.0.1",
+    UserName = "admin",
+    Password = "admin",
+    VirtualHost = "my_vhost"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine("收到消息 {0}", message);
+    };
+    channel.BasicConsume(queue: "mymessage",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" 按回车退出");
+    Console.ReadLine();
+}
---
+++ src/DirectSendDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "127.0.0.1",
+    UserName = "admin",
+    Password = "admin",
+    VirtualHost = "my_vhost"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "mymessage",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+    Console.WriteLine("输入需要传输的消息，输入Exit退出");
+    var message = Console.ReadLine();
+    while (message != "Exit")
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: "mymessage",
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine(" 发送消息 {0}", message);
+        message = Console.ReadLine();
+    }
+}
+
+Console.WriteLine("按回车退出");
+Console.ReadLine();
+
