82a5ed3de9f9b89a70d6a540b60731dbab2887bf
甄镭 <zhenlei1970@hotmail.com>
2021-12-27 15:31:37 +0800

    add direct

---
+++ DirectRouteKeyReceiver1/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "127.0.0.1",
+    UserName = "admin",
+    Password = "admin",
+    VirtualHost = "my_vhost"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+
+    channel.ExchangeDeclare(exchange: "directdemo.exchange",
+type: ExchangeType.Direct, durable: true);
+
+    channel.QueueDeclare(queue: "log_que",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    channel.QueueBind(queue: "log_que", exchange: "directdemo.exchange",
+routingKey: "log");
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine("收到消息 {0}", message);
+    };
+    channel.BasicConsume(queue: "log_que",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" 按回车退出");
+    Console.ReadLine();
+}
---
+++ DirectRouteKeySender/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "127.0.0.1",
+    UserName = "admin",
+    Password = "admin",
+    VirtualHost = "my_vhost"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare("directdemo.exchange", ExchangeType.Direct, true, false, null);
+
+    Console.WriteLine("输入需要传输的消息，输入Exit退出");
+    var message = Console.ReadLine();
+    while (message != "Exit")
+    {
+        Console.WriteLine("输入RouteKey");
+        var routekey = Console.ReadLine();
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "directdemo.exchange",
+                             routingKey: routekey,
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine(" 发送消息 {0} Routekey {1}", message,routekey);
+        message = Console.ReadLine();
+    }
+}
+
+Console.WriteLine("按回车退出");
+Console.ReadLine();
